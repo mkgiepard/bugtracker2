ecac078a8cb7030ccb257e8b66f42b58f7c3d744
Mariusz <mkgiepard@gmail.com>
2022-01-25 20:52:58 +0100

    bug-report-create: add initial code, lots todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from './modules/material/material.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,6 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
--- src/app/components/bug-report-create/bug-report-create.component.ts
+++ src/app/components/bug-report-create/bug-report-create.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bug-report-create',
   templateUrl: './bug-report-create.component.html',
-  styleUrls: ['./bug-report-create.component.css']
+  styleUrls: ['./bug-report-create.component.css'],
 })
 export class BugReportCreateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  emptyBugReport: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.emptyBugReport = fb.group({});
   }
 
+  ngOnInit(): void {}
 }
