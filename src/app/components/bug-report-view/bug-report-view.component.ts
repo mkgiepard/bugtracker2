7e39459df47eca84655ad47b4931f798f95f3de7
Mariusz <mkgiepard@gmail.com>
2022-01-31 20:45:55 +0100

    bug-report-list/view: pass bug id while navigating between the list and a view

--- src/app/components/bug-report-view/bug-report-view.component.ts
+++ src/app/components/bug-report-view/bug-report-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BugReport, bugReportData } from 'src/app/dataModel/bug-report';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-bug-report-view',
@@ -8,24 +9,34 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./bug-report-view.component.css'],
 })
 export class BugReportViewComponent implements OnInit {
-  bugReport = bugReportData[0];
+  bugReport: BugReport | undefined;
   bugReportForm: FormGroup = new FormGroup({
     title: new FormControl(),
-    priority: new FormControl('p' + this.bugReport.priority),
+    priority: new FormControl(),
     description: new FormControl(),
   });
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const bugIdFromRoute = Number(routeParams.get('id'));
+
+    // Find the product that correspond with the id provided in route.
+    this.bugReport = bugReportData.find(bugReport => bugReport.bug_id === bugIdFromRoute);
+    this.bugReportForm.patchValue({ priority: 'p' + this.bugReport?.priority });
+  }
 
   upPriority() {
+    if (this.bugReport == undefined) return;
     if (this.bugReport.priority == 0) return;
     this.bugReport.priority--;
     this.bugReportForm.patchValue({ priority: 'p' + this.bugReport.priority });
   }
 
   downPriority() {
+    if (this.bugReport == undefined) return;
     if (this.bugReport.priority == 4) return;
     this.bugReport.priority++;
     this.bugReportForm.patchValue({ priority: 'p' + this.bugReport.priority });
