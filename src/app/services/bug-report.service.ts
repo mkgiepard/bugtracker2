5af5358cd81527518813fac98771fbbdce9c65ae
Mariusz <mkgiepard@gmail.com>
2022-02-25 20:20:55 +0100

    bug-report-service: add error handling

--- src/app/services/bug-report.service.ts
+++ src/app/services/bug-report.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { BugReport, bugReportData } from '../dataModel/bug-report';
 
@@ -13,6 +14,30 @@ export class BugReportService {
   constructor(private http: HttpClient) { }
 
   getBugReports(): Observable<BugReport[]> {
-    return this.http.get<BugReport[]>(this.bugReportUrl);
+    return this.http.get<BugReport[]>(this.bugReportUrl)
+      .pipe(
+        catchError(this.handleError<BugReport[]>('getBugReports', []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      //this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
