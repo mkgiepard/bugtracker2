db6a5b89d83426cbbd542c702949e05b2839ab44
Mariusz <mkgiepard@gmail.com>
2022-03-13 18:15:08 +0100

    bug-report-service: adding more tests

--- src/app/services/bug-report.service.spec.ts
+++ src/app/services/bug-report.service.spec.ts
@@ -4,6 +4,8 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from "@angular/common/http/testing";
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { createSpyFromClass, Spy } from 'jasmine-auto-spies';
 
 import { BugReportService } from './bug-report.service';
 import { BugReport, Status } from '../dataModel/bug-report';
@@ -11,13 +13,22 @@ import { BugReport, Status } from '../dataModel/bug-report';
 describe('BugReportService', () => {
   let service: BugReportService;
   let httpTestingController: HttpTestingController;
-  let mockBugReports: BugReport[];
+  let httpSpy: Spy<HttpClient>;
+  let fakeBugReports: BugReport[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        BugReportService,
+        { provide: HttpClient, useValue: createSpyFromClass(HttpClient) }
+      ]
+    });
     service = TestBed.inject(BugReportService);
+    httpSpy = TestBed.inject<any>(HttpClient);
+
     httpTestingController = TestBed.get(HttpTestingController);
-    mockBugReports = [
+    fakeBugReports = [
       {
         id: 1001,
         title: 'highest priority',
@@ -41,15 +52,73 @@ describe('BugReportService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should GET a list of bugReports', () => {
-    service.getBugReports().subscribe((bugReports) => {
-      expect(bugReports.length).toBe(2);
-      expect(bugReports).toEqual(mockBugReports);
+  it('#getBugReports should return an expected list of bugReports', (done: DoneFn) => {
+    httpSpy.get.and.nextWith(fakeBugReports);
+
+    service.getBugReports().subscribe({
+      next: bugReports => {
+        expect(bugReports).toHaveSize(fakeBugReports.length);
+        done();
+      },
+      error: done.fail
     });
-    const request = httpTestingController.expectOne("api/bugReports");
-    expect(request.request.method).toBe("GET");
-    request.flush(mockBugReports);
-    httpTestingController.verify();
+    expect(httpSpy.get.calls.count()).toBe(1);
+  });
+
+  it('#addBugReport should create a new bugReport', (done: DoneFn) => {
+
+    var newBugReport = {
+      id: 1003,
+      title: 'dummyy',
+      priority: 2,
+      status: Status.New,
+      description: 'lorem epsum...',
+      author: 'Buggy Bug',
+    } as BugReport;
+
+    httpSpy.post.and.nextWith(newBugReport);
+
+    service.addBugReport(newBugReport).subscribe({
+      next: bugReport => {
+        expect(bugReport).toEqual(newBugReport);
+        done();
+      },
+      error: done.fail
+    });
+    expect(httpSpy.post.calls.count()).toBe(1);
+  });
+
+  it('#updateBugReport should update a bugReport with given id', (done: DoneFn) => {
+
+    var bugReport = fakeBugReports[0];
+    bugReport.title = "Updated";
+
+    httpSpy.put.and.nextWith(bugReport);
+
+    service.updateBugReport(bugReport).subscribe({
+      next: bugReport => {
+        expect(bugReport.title).toEqual("Updated");
+        done();
+      },
+      error: done.fail
+    });
+    expect(httpSpy.put.calls.count()).toBe(1);
+  });
+
+  it('#deleteBugReport should delete an existing bugReport', (done: DoneFn) => {
+
+    httpSpy.delete.and.nextWith(new HttpResponse({
+      status: 200
+    }));
+
+    service.deleteBugReport(1001).subscribe(
+      response => {
+        expect(response).toEqual({ status: 200 });
+        done();
+      },
+      done.fail
+    );
+    expect(httpSpy.delete.calls.count()).toBe(1);
   });
 
   it('should increase bugReport priority from 4 to 0', () => {
