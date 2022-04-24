fcada7d881ada017cf7be810fa49cd1b2076a684
Mariusz <mkgiepard@gmail.com>
2022-04-24 16:21:45 +0200

    bug-report-add-comment: fix the test

--- src/app/components/bug-report-add-comment/bug-report-add-comment.component.spec.ts
+++ src/app/components/bug-report-add-comment/bug-report-add-comment.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BugReportAddCommentComponent } from './bug-report-add-comment.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+// Http testing module and mocking controller
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { MaterialModule } from '../../modules/material/material.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('BugReportAddCommentComponent', () => {
   let component: BugReportAddCommentComponent;
@@ -8,9 +17,17 @@ describe('BugReportAddCommentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BugReportAddCommentComponent ]
+      declarations: [BugReportAddCommentComponent],
+      imports: [
+        HttpClientTestingModule,
+        MaterialModule,
+        // NoopAnimationsModule,
+        // ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
