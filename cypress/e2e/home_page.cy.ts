f6343903741674a458ea7909fd6a787fa781841b
Mariusz <mkgiepard@gmail.com>
2023-05-28 17:09:29 +0200

    e2e> adding a test for navigation to edit page

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,22 +1,28 @@
 import { HomePage } from '../pages/home_page';
+import { EditPage } from '../pages/edit_page';
 
 const homePage = new HomePage();
 
-describe("The Home Page", () => {
+describe('The Home Page', () => {
   beforeEach(() => {
-    homePage.open();
-  })
-
-  it("Navigation: successfully loads", () => {
     homePage.open();
   });
 
-  it("Navigation: FAB should open new bug report page", () => {
+  it('Navigation: successfully loads', () => {
+    homePage.open();
+  });
+
+  it('Navigation: FAB should open new bug report page', () => {
     homePage.clickFAB();
   });
 
-  it("Navigation: click on a title should open bug report view page", () => {
-    homePage.clickOnBugTitle("bug report 2");
+  it('Navigation: click on a title should open bug report view page', () => {
+    homePage.clickOnBugTitle('bug report 2');
+  });
+
+  it('Navigation: click on a edit button should open bug report edit page', () => {
+    const edit = homePage.clickOnEditInRow(2);
+    edit.validateEditPageForId(2);
   });
 
   it("Action: click on 'up' should increase a bug priority", () => {
@@ -28,6 +34,4 @@ describe("The Home Page", () => {
     homePage.clickOnDownPriorityInRow(1);
     homePage.validatePriorityInRow(1, 1);
   });
-
-
 });
--- cypress/pages/edit_page.ts
+++ cypress/pages/edit_page.ts
@@ -1,35 +1,39 @@
-import { HomePage } from "./home_page";
+import { HomePage } from './home_page';
 
 export class EditPage {
+  open(id: number) {
+    cy.visit('/edit/' + id);
+    cy.url().should('include', '/edit/' + id);
+  }
 
-    open(id: number) {
-        cy.visit("/edit/" + id);
-        cy.url().should("include", "/edit/" + id);
-    }
+  clickOnDelete() {
+    cy.get('mat-icon').eq(5).click();
+    cy.url().should('include', '/list');
+  }
 
-    clickOnDelete() {
-        cy.get('mat-icon').eq(5).click();
-        cy.url().should("include", "/list");
-    }
+  clickOnBack(): HomePage {
+    cy.contains('Back').click();
+    return new HomePage();
+  }
 
-    clickOnBack(): HomePage {
-        cy.contains('Back').click();
-        return new HomePage();
-    }
+  clickOnSave(): HomePage {
+    cy.contains('Save').click();
+    return new HomePage();
+  }
 
-    clickOnSave(): HomePage {
-        cy.contains('Save').click();
-        return new HomePage();
-    }
+  updateTitle(newTitle: string) {
+    cy.get("[formcontrolname='title']").clear();
+    cy.get("[formcontrolname='title']").type(newTitle);
+  }
 
-    updateTitle(newTitle: string) {
-        cy.get("[formcontrolname='title']").clear();
-        cy.get("[formcontrolname='title']").type(newTitle);
-    }
+  validateEditPage() {
+    cy.url().should('include', '/edit');
+    cy.contains('Save');
+  }
 
-    validateEditPage() {
-        cy.url().should("include", "/edit");
-        cy.contains("Save");
-    }
-
+  validateEditPageForId(id: number) {
+    const idWithOffset = 1000 + id;
+    this.validateEditPage();
+    cy.url().should('include', '/edit/' + idWithOffset);
+  }
 }
