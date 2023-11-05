bb979caa5ae1d1a3b226c60302710e547ae7a8ec
Mariusz <mkgiepard@gmail.com>
2023-11-05 11:45:06 +0100

    test> add a test for clicking fab in BugReportList

--- src/app/components/bug-report-list/bug-report-list.component.spec.ts
+++ src/app/components/bug-report-list/bug-report-list.component.spec.ts
@@ -3,17 +3,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BugReportListComponent } from './bug-report-list.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 // Http testing module and mocking controller
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { MaterialModule } from '../../modules/material/material.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('BugReportListComponent', () => {
   let component: BugReportListComponent;
   let fixture: ComponentFixture<BugReportListComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BugReportListComponent],
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+        MaterialModule,
+        NoopAnimationsModule,
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
@@ -21,6 +31,7 @@ describe('BugReportListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BugReportListComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
@@ -34,7 +45,15 @@ describe('BugReportListComponent', () => {
 
   xit('should display column with action buttons', () => {});
 
-  xit('should navigate to create when "FAB" is clicked', () => {});
+  it('should navigate to create when "FAB" is clicked', () => {
+    const navSpy = spyOn(router, 'navigateByUrl');
+    const fab = getMatIconElement('add');
+
+    fab.click();
+
+    expect(navSpy).toHaveBeenCalledTimes(1);
+    expect(navSpy.calls.mostRecent().args[0].toString()).toEqual('/create');
+  });
 
   xit('should navigate to /comment when "Comment" button is clicked', () => {});
 
@@ -53,4 +72,18 @@ describe('BugReportListComponent', () => {
   xit('should call delete() method when "Delete" button is clicked', () => {});
 
   xit('should display search input filed', () => {});
+
+  function getMatIconElement(text: string): HTMLElement {
+    const icons = fixture.nativeElement.querySelectorAll(
+      'mat-icon'
+    ) as HTMLElement[];
+    const icon = Array.from(icons).find(
+      (el) => el.innerText === text
+    ) as HTMLElement;
+    // Needed to prevent 'ERROR: Some of your tests did a full page reload!'
+    icon.addEventListener('click', function (e) {
+      e.preventDefault();
+    });
+    return icon;
+  }
 });
