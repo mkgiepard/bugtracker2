c16729bd58a8f3d4cc8e8671b2bd07c2d458a882
Mariusz <mkgiepard@gmail.com>
2024-01-04 18:31:56 +0100

    test> fix tests in register component

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { MaterialModule } from '../../modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,10 +11,10 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
-    })
-    .compileComponents();
-    
+      declarations: [RegisterComponent],
+      imports: [MaterialModule, ReactiveFormsModule, NoopAnimationsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
