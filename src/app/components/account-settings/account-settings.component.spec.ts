b5bbe48e7e4f589b4eb5149b4f4b80b65dc69437
Mariusz <mkgiepard@gmail.com>
2024-04-03 19:24:30 +0200

    tset> fix tests after adding a form in account settings page

--- src/app/components/account-settings/account-settings.component.spec.ts
+++ src/app/components/account-settings/account-settings.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MaterialModule } from '../../modules/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AccountSettingsComponent } from './account-settings.component';
 
 describe('AccountSettingsComponent', () => {
@@ -8,9 +11,9 @@ describe('AccountSettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AccountSettingsComponent ]
-    })
-    .compileComponents();
+      declarations: [AccountSettingsComponent],
+      imports: [MaterialModule, ReactiveFormsModule, NoopAnimationsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AccountSettingsComponent);
     component = fixture.componentInstance;
