5bd64efac75d071cdcabb65a2315e36058f2792e
Mariusz <mkgiepard@gmail.com>
2024-04-03 14:20:43 +0200

    new> implement account settings page (template only)

--- src/app/components/account-settings/account-settings.component.ts
+++ src/app/components/account-settings/account-settings.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
-  styleUrls: ['./account-settings.component.css']
+  styleUrls: ['./account-settings.component.css'],
 })
 export class AccountSettingsComponent {
+  settingsForm: FormGroup | undefined;
 
+  ngOnInit(): void {
+    this.settingsForm = new FormGroup({
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+    });
+  }
 }
