cbed006fd98ea77096ebe06c4b8ddd1fe65d1a91
Mariusz <mkgiepard@gmail.com>
2024-04-13 14:58:32 +0200

    refactor> switch from template based forms to reactive forms in bugReportEdit

--- src/app/components/bug-report-edit/bug-report-edit.component.ts
+++ src/app/components/bug-report-edit/bug-report-edit.component.ts
@@ -3,6 +3,7 @@ import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { BugReport, Status } from 'src/app/dataModel/bug-report';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BugReportService } from 'src/app/services/bug-report.service';
+import { provideImageKitLoader } from '@angular/common';
 
 @Component({
   selector: 'app-bug-report-edit',
@@ -39,12 +40,24 @@ export class BugReportEditComponent implements OnInit {
   }
 
   getBugReport(id: number) {
-    this.bugReportService
-      .getBugReport(id)
-      .subscribe((bugReport) => (this.bugReport = bugReport));
+    this.bugReportService.getBugReport(id).subscribe((bugReport) => {
+      this.bugReport = bugReport;
+      this.bugReportForm!.patchValue({
+        id: this.bugReport.id,
+        title: this.bugReport.title,
+        status: this.bugReport.status,
+        priority: this.bugReport.priority,
+        description: this.bugReport.description,
+      });
+    });
   }
 
   upPriority() {
+    if (this.bugReport == undefined) return;
+    if (this.bugReport.priority != 0) {
+      const priority = this.bugReport.priority - 1;
+      this.bugReportForm!.patchValue({ priority: priority });
+    }
     if (this.bugReport == undefined) return;
     this.bugReportService
       .upPriority(this.bugReport)
@@ -53,20 +66,34 @@ export class BugReportEditComponent implements OnInit {
 
   downPriority() {
     if (this.bugReport == undefined) return;
-    this.bugReportService.downPriority(this.bugReport).subscribe();
+    if (this.bugReport.priority != 4) {
+      const priority = this.bugReport.priority + 1;
+      this.bugReportForm!.patchValue({ priority: priority });
+    }
+    if (this.bugReport == undefined) return;
+    this.bugReportService
+      .downPriority(this.bugReport)
+      .subscribe(() => this.reload());
   }
 
   markAsFixed() {
+    this.bugReport = Object.assign(this.bugReport!, this.bugReportForm.value);
     if (this.bugReport == undefined) return;
-    this.bugReportService.markAsFixed(this.bugReport).subscribe();
+    this.bugReportService
+      .markAsFixed(this.bugReport)
+      .subscribe(() => this.reload());
   }
 
   markAsWnf() {
+    this.bugReport = Object.assign(this.bugReport!, this.bugReportForm.value);
     if (this.bugReport == undefined) return;
-    this.bugReportService.markAsWnf(this.bugReport).subscribe();
+    this.bugReportService
+      .markAsWnf(this.bugReport)
+      .subscribe(() => this.reload());
   }
 
   save(): void {
+    this.bugReport = Object.assign(this.bugReport!, this.bugReportForm.value);
     if (this.bugReport) {
       this.bugReportService
         .updateBugReport(this.bugReport)
@@ -75,6 +102,7 @@ export class BugReportEditComponent implements OnInit {
   }
 
   delete(): void {
+    this.bugReport = Object.assign(this.bugReport!, this.bugReportForm.value);
     if (
       this.bugReport &&
       window.confirm(
@@ -92,6 +120,7 @@ export class BugReportEditComponent implements OnInit {
   }
 
   reload(): void {
+    this.bugReport = Object.assign(this.bugReport!, this.bugReportForm.value);
     if (this.bugReport == undefined) return;
     this.getBugReport(this.bugReport.id);
   }
