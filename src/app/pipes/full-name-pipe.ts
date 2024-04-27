bb64a574422c1ce00fd260fefe3ef0ed37fe738d
Mariusz <mkgiepard@gmail.com>
2024-04-27 11:12:27 +0200

    fix> fix the displayName while user has no firstname/lastname defined

--- src/app/pipes/full-name-pipe.ts
+++ src/app/pipes/full-name-pipe.ts
@@ -6,6 +6,17 @@ import { User } from '../dataModel/user';
 })
 export class FullNamePipe implements PipeTransform {
   transform(person: User): string {
-    return person.firstName + ' ' + person.lastName;
+    let displayName = person.username;
+    if (person.firstName && !person.lastName) {
+      displayName = person.firstName;
+    }
+    if (!person.firstName && person.lastName) {
+      displayName = person.lastName;
+    }
+    if (person.firstName && person.lastName) {
+      displayName = person.firstName + ' ' + person.lastName
+    }
+
+    return displayName;
   }
 }
